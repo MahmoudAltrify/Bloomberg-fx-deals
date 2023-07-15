e756969865117b4da829c2082ae4456464abcc1d
Mahmoud Hassan Al-Trify <Mahmoud.altrify@gmail.com>
2023-07-15 12:58:08 +0300

    initialization: create springboot project with maven

---
+++ src/test/java/com/progresssoft/fxdeals/FxDealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progresssoft.fxdeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxDealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
